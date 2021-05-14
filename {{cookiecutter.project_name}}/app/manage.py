2f90a91bc901025620618457da79941518bc727c
Artem Innokentiev <artinnok@users.noreply.github.com>
2021-05-14 15:36:22 +0900

    Changed DJANGO_SETTINGS_MODULE in  `manage.py`

--- {{cookiecutter.project_name}}/app/manage.py
+++ {{cookiecutter.project_name}}/app/manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
